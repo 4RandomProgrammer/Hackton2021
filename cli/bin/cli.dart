9a4d8fab0bfe60f3b86b635df623323bc1d77cc7
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2021-03-14 22:05:52 -0300

    Tentativa

---
+++ cli/bin/cli.dart
@@ -0,0 +1,14 @@
+import 'package:cli/cli.dart' as cli;
+import 'dart:io';
+
+void main() {
+  //O que implementar:
+  //- soma, subtração, multiplicação, divisão (operações básicas)
+  //- raiz quadrada, log (pelo menos em base 10)
+
+  print('Seja bem vindo a calculadora!');
+  while (true) {
+    print('Digite o comando a ser realizado?');
+    
+  }
+}
